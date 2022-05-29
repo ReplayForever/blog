3455a878e4893766b2a16592f20c4472dcf8189a
leonid <maslovskie77@gmail.com>
2022-05-29 18:39:01 +0300

    add templates in news

--- news/views.py
+++ news/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from news.models import News
+
 
 def index(request):
-    return HttpResponse('News')
-
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
